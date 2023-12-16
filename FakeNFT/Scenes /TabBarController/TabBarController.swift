941dbc09c0df41f3fa9ad91b0b1ec8d0aa6dd050
mariia <mkhavantseva@gmail.com>
2023-12-16 16:48:30 +0300

    добавила таббар из комок

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -9,19 +9,19 @@ final class TabBarController: UITabBarController {
         image: UIImage(systemName: "square.stack.3d.up.fill"),
         tag: 1
     )
-    
+
     private let profileTabBarItem = UITabBarItem(
         title: NSLocalizedString("Tab.profile", comment: ""),
         image: UIImage(named: "Profile"),
         tag: 0
     )
-    
+
     private let statisticsTabBarItem = UITabBarItem(
         title: NSLocalizedString("Tab.statistics", comment: ""),
         image: UIImage(named: "tabBarStatImages"),
         tag: 3
     )
-    
+
     private let basketTabBarItem = UITabBarItem(
         title: NSLocalizedString("Tab.basket", comment: ""),
         image: UIImage(named: "Basket"),
@@ -40,35 +40,31 @@ final class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let servicesAssembly = servicesAssembly else { return }
-        
+
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
         catalogController.tabBarItem = catalogTabBarItem
-        
+
         let profileController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
         profileController.tabBarItem = profileTabBarItem
-        
-        
+
         let basketController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
         basketController.tabBarItem = basketTabBarItem
-        
-        let statisticsController = StatisticsViewController(
-            servicesAssembly: servicesAssembly
-        )
-        statisticsController.tabBarItem = statisticsTabBarItem
-        
-        let statisticsNavController = UINavigationController(rootViewController: statisticsController)
 
-        viewControllers = [profileController, catalogController, basketController, statisticsController]
+        let statisticsController = StatisticsViewController(servicesAssembly: servicesAssembly)
+           statisticsController.tabBarItem = statisticsTabBarItem
+           let statisticsNavController = UINavigationController(rootViewController: statisticsController)
+
+        viewControllers = [profileController, catalogController, basketController, statisticsNavController]
 
         view.backgroundColor = .ypWhite
         tabBar.unselectedItemTintColor = .ypBlack
-        
+
     }
 
 }
